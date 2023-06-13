e602fcde39a24bc952740b370dfdf78efc3f5b5f
nottabitha <leimonistabitha@gmail.com>
2023-06-13 19:45:13 +0800

    initial commit

---
+++ Blink.cs
@@ -0,0 +1,23 @@
+﻿using System.Device.Gpio;
+
+namespace BlinkTutorial
+{
+    public class Blink
+    {
+        public static async Task BlinkMillisecond(int ms, int pin)
+        {
+            Console.WriteLine("Blinking LED. Press Ctrl+C to end.");
+            using var controller = new GpioController();
+            controller.OpenPin(pin, PinMode.Output);
+            bool ledOn = true;
+            while (true)
+            {
+                controller.Write(pin, ledOn ? PinValue.High : PinValue.Low);
+                await Task.Delay(ms);
+                ledOn = !ledOn;
+            }
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using BlinkTutorial;
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+
+        await Blink.BlinkMillisecond(1000,18);
+    }
+    
+}
+
+
+
+
